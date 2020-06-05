99532a126835de0149357ea21e682b82e3dc531d
Jamena McInteer <ndjamenamarmon@gmail.com>
2020-06-05 14:29:47 -0500

    Implement phone number component with test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import IntlTelInput from "react-intl-tel-input";
+import "react-intl-tel-input/dist/main.css";
 
 function App() {
+  const [phoneNumberValidation, setPhoneNumberValidation] = useState();
+
+  const onPhoneNumberBlur = (isValid) => {
+    setPhoneNumberValidation(
+      isValid ? "Phone number is valid" : "Phone number is invalid"
+    );
+  };
+
+  const onSelectFlag = (isValid) => {
+    setPhoneNumberValidation(
+      isValid ? "Phone number is valid" : "Phone number is invalid"
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <label htmlFor="phoneNumber" className="form-label">
+        Phone Number
+      </label>
+      <IntlTelInput
+        fieldId="phoneNumber"
+        defaultValue="234-567-8901"
+        containerClassName="intl-tel-input"
+        inputClassName="form-control"
+        preferredCountries={["us"]}
+        separateDialCode={true}
+        format={true}
+        onPhoneNumberBlur={onPhoneNumberBlur}
+        onSelectFlag={onSelectFlag}
+      />
+      {phoneNumberValidation && (
+        <div className="form-error">{phoneNumberValidation}</div>
+      )}
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, fireEvent, wait } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("validates phone number", async () => {
+  const { getByText, getByLabelText } = render(<App />);
+
+  // fireEvent.change(getByLabelText("Phone Number"), {
+  //   target: { value: "(234) 567-0000" },
+  // });
+  fireEvent.click(getByLabelText("Phone Number"));
+  fireEvent.blur(getByLabelText("Phone Number"));
+  await wait(() => getByText(/Phone number is valid/));
 });
